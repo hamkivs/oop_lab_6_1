c980307a5e411a2c0d7e1536720c5b2e2fcfe321
gamki <gamki@LAPTOP-7NBRE5R5>
2024-12-09 17:55:42 +0200

    Add project files.

---
+++ oop_lab_6_1/oop_lab_6_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath> // для std::abs, якщо потрібен універсальний підхід
+
+// Шаблонна функція для обчислення модуля
+template <typename T>
+T absValue(T value) {
+    return value < 0 ? -value : value;
+}
+
+int main() {
+    int intValue = -5;
+    double doubleValue = -3.14;
+
+    std::cout << "Modul: " << intValue << " = " << absValue(intValue) << std::endl;
+    std::cout << "Modul: " << doubleValue << " = " << absValue(doubleValue) << std::endl;
+
+    return 0;
+}
